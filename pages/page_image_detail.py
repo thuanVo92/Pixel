22d8494fb922ae8e2bb198aef97046946fca74e2
Thuan Vo <thithuan.it92@gmail.com>
2021-10-10 01:58:36 +0700

    Create page_image_detail.py
    page iamge detail

---
+++ pages/page_image_detail.py
@@ -0,0 +1,28 @@
+'''
+Created on Oct 7, 2021
+
+@author: DELL
+'''
+def icon_user():
+    return "//header/div[1]/span[1]/div[1]/span[1]/div[1]/a[1]/div[1]/div[1]/img[1]"
+
+def btn_follow():
+    return "button[title='Follow"
+
+def btn_following():
+    return "button[title='Following"
+
+def a():
+    return "//body/div[4]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]"
+
+def btn_download():
+    return "//span[contains(text(),'Download')]"
+
+def dialog_thank():
+    return "//body/div[@id='app']/div[1]/div[5]/div[1]/div[1]"
+
+def msg_follow():
+    return "//body/div[@id='app']/div[1]/div[3]/div[1]/button[1]/*[1]"
+
+def name_owner ():
+    return "//header/div[1]/span[1]/div[2]/span[1]/div[1]/a[1]"
