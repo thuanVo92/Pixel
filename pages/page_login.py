8ccee436334fc55d9454fd588e001b30dd090f21
Thuan Vo <thithuan.it92@gmail.com>
2021-10-10 01:58:52 +0700

    Create page_login.py
    page login

---
+++ pages/page_login.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 4, 2021
+
+@author: DELL
+'''
+def txt_email():
+    return "//input[@id='user_email']"
+
+def txt_password():
+    return "//input[@id='user_password']"
+
+def btn_login():
+    return "//body/div[2]/div[1]/div[1]/div[1]/div[1]/form[1]/div[3]/input[1]"
+
+def icon_login():
+    return "//h1[contains(text(),'Login')]"
+
